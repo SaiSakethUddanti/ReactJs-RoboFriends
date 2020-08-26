9f9eb90b02155a283f8a3d75486d1dc232918a47
Sai Saketh <uddantisaketh2001@gmail.com>
2020-08-26 14:12:04 +0530

    Updates

---
+++ precache-manifest.06d184beaba08ebca5faebfc7c53c67c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3488744292cb1f342c2d733bceb7e66d",
+    "url": "/ReactJs-RoboFriends/index.html"
+  },
+  {
+    "revision": "7d65e0d887d1a91fcd1f",
+    "url": "/ReactJs-RoboFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "eb263871d5cc9ab589ba",
+    "url": "/ReactJs-RoboFriends/static/css/main.167c2ef7.chunk.css"
+  },
+  {
+    "revision": "7d65e0d887d1a91fcd1f",
+    "url": "/ReactJs-RoboFriends/static/js/2.b52811a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactJs-RoboFriends/static/js/2.b52811a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb263871d5cc9ab589ba",
+    "url": "/ReactJs-RoboFriends/static/js/main.fe1e09ed.chunk.js"
+  },
+  {
+    "revision": "162f4b3c4bf1f6c6f29c",
+    "url": "/ReactJs-RoboFriends/static/js/runtime-main.bfaa07ef.js"
+  }
+]);
